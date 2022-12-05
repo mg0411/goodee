4cc7dd6d246384f6f6433db7f73618d4b7ee2bf0
mg0411 <118701129+mg0411@users.noreply.github.com>
2022-12-05 12:21:47 +0900

    Add files via upload

---
+++ kisok/Aaa.java
@@ -0,0 +1,16 @@
+package com.Kisok;
+
+public class Aaa {
+	final  String DOT="*";
+	public void line() {
+		for(int i= 0 ; i < 32 ; i++ ) {
+			Common.dw(DOT);
+		}
+		Common.dw("\n");
+		System.out.println("************고양이 카페************");
+		Common.dw("********************************");
+		Common.dw("\n");
+	}
+	
+	
+}
---
+++ kisok/Main.java
@@ -0,0 +1,16 @@
+package com.Kisok;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Aaa st = new Aaa();
+		st.line();
+		kisok.run();
+					
+				}
+	}
+
+
+				
+				
+				
